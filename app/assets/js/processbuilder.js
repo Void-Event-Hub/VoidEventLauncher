98a91d776362a52dd42e9dedfd10b8c2f6c7c15c
patryk3211 <patrykmierzy@gmail.com>
2024-09-07 15:22:15 +0200

    Fix process environment not propagating to java

--- app/assets/js/processbuilder.js
+++ app/assets/js/processbuilder.js
@@ -74,15 +74,13 @@ class ProcessBuilder {
         logger.info('Launch Arguments:', args)
 
         if (process.env['_JAVA_OPTIONS']) {
+            process.env['_JAVA_OPTIONS'] = ''
             logger.warn('Detected _JAVA_OPTIONS environment variable. This can cause issues with the launcher, so it has been overridden.')
         }
 
         const child = child_process.spawn(ConfigManager.getJavaExecutable(this.server.rawServer.id), args, {
             cwd: this.gameDir,
-            detached: ConfigManager.getLaunchDetached(),
-            env: {
-                '_JAVA_OPTIONS': ''
-            }
+            detached: ConfigManager.getLaunchDetached()
         })
 
         if(ConfigManager.getLaunchDetached()){
